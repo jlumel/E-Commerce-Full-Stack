1e167a928a1cc4ccd90981db77a819d8b7d2ec6b
Joaquin Lumelsky <jlumelsky@gmail.com>
2021-06-08 03:13:53 -0300

    terceraentrega

--- src/service/twilio.service.ts
+++ src/service/twilio.service.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import twilio from 'twilio'
-import {User} from '../models/users.model'
+import { User } from '../models/users.model'
 dotenv.config()
 
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
@@ -8,7 +8,7 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 export const sendWhatsapp = (user: User) => {
     client.messages.create({
         body: `Nuevo pedido de ${user.username}-${user.email}`,
-        from: 'whatsapp:' + process.env.TWILIO_NUMBER,
+        from: 'whatsapp:' + process.env.TWILIO_WHATSAPP_NUMBER,
         to: 'whatsapp:' + process.env.ADMIN_PHONE_NUMBER
     })
 }
